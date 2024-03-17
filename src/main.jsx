120c1d29a0a95e8d77b9dcd4fb1279b3dae2c3a7
Ianssogothis <mount88ian@gmail.com>
2024-03-17 13:02:44 -0700

    initial ONE

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import "vite/modulepreload-polyfill";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import 'semantic-ui-css/semantic.min.css' // import
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
